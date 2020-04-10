97d17b0a79d5d8824a97ff7e849a83daf0bda334
Jakub Krysztofiak <jakub.krysztofiak@protonmail.com>
2020-04-10 15:33:39 +0200

    LetterCount and single perceptron training

--- singleLayerNeuralNetwork.py
+++ singleLayerNeuralNetwork.py
@@ -1,9 +1,33 @@
 import os
+import Perceptron
+import string
+
+
 
 #TODO: Wczytanie pliku i konwersja na 26-elementowy wektor proporcji liter 
+
+def countLetters(text: str) -> list:
+    
+    letter_count = dict.fromkeys(string.ascii_lowercase, 0)
+    for l in text:
+        if l.lower() in letter_count:
+            letter_count[l.lower()]+=1
+
+    letters = [] 
+    for key in sorted(letter_count):
+        letters.append(letter_count[key])
+
+    return letters
+
+
+
 path = 'data'
 
+p1 = Perceptron.Perceptron(26,"English")
+
+
 for root, subdirs, files in os.walk(path):
+    group = root[5:]
     for file in files:
         if '.txt' in file:
             tmpPath = os.path.join(root,file)
@@ -11,15 +35,10 @@ for root, subdirs, files in os.walk(path):
             with open(tmpPath, 'r') as f:
                 text = f.read()
 
-            letters = {}
+            letters = countLetters(text)
+    
+            p1.training(letters,group, 0.25)
 
-            for l in text:
-                if ord(l) in range(ord('A'),ord('Z')+1) or ord(l) in range(ord('a'),ord('z')+1):
-                    if l.lower() in letters:
-                        letters[l.lower()]+=1
-                    else:
-                        letters[l.lower()] = 1
-                
 
 
 #TODO: Trenowanie perceptronu n do rozpoznawania tylko n-języka 
