ebca10d8f721349f32087f5cbb930460f2702309
Jakub Krysztofiak <jakub.krysztofiak@protonmail.com>
2020-04-10 21:24:29 +0200

    removed hardcoded variables

--- singleLayerNeuralNetwork.py
+++ singleLayerNeuralNetwork.py
@@ -1,17 +1,20 @@
 import os
 import Perceptron
 import string
+import random
 
 
 def countLetters(text: str) -> list:
     letter_count = dict.fromkeys(string.ascii_lowercase, 0)
+    allLetters = 0
     for l in text:
         if l.lower() in letter_count:
             letter_count[l.lower()]+=1
+            allLetters +=1 
 
     letters = [] 
     for key in sorted(letter_count):
-        letters.append(letter_count[key])
+        letters.append(letter_count[key]/allLetters)
 
     return letters
 
@@ -21,52 +24,44 @@ def countLetters(text: str) -> list:
 
 path = 'data'
 
-p1 = Perceptron.Perceptron(26,"English")
-p2 = Perceptron.Perceptron(26,"Italian")
-p3 = Perceptron.Perceptron(26,"German")
-
 dirs = []
+groups = []
 
 for root, subdirs, files in os.walk(path):
+    if root[5:] not in groups and root[5:]!="":
+        groups.append(root[5:])
     for file in files:
         if '.txt' in file:
             dirs.append(os.path.join(root,file))
 
-order = [0,5,17,18,8,23,9,21,11,3,22,12,13,2,4,14,15,1,16,7,19,6,20,10]
+perceptrons = []
+for g in groups:
+    perceptrons.append(Perceptron.Perceptron(26,g))
 
-for x in order:
-    print(dirs[x])
+random.shuffle(dirs)
+
+for file in dirs:
     group = ""
-    if "English" in dirs[x]:
-        group = "English"
-    if "Italian" in dirs[x]:
-        group = "Italian"
-    if "German" in dirs[x]:
-        group = "German"
+    for g in groups:
+        if g in file:
+            group = g
+
+    print(file, "is group: "+group)
             
-    with open(dirs[x], 'r') as f:
+    with open(file, 'r') as f:
         text = f.read()
 
     letters = countLetters(text)
 
-    # print(f"Group: {group}")
-    # print(f"Results: ")
-    
-    print("TRAIN")
-    p1.training(letters,group, 4.25)
-    p2.training(letters,group, 1)
-    p3.training(letters,group, 3.75)
-    # print("TEST")
-    # print(p1.testing(letters))
-    # print(p2.testing(letters))
-    # print(p3.testing(letters))
+    for per in perceptrons:
+        per.training(letters, group, 0.5)
 
+    for per in perceptrons:
+        print(f"{per.group}: ",per.testing(letters))
 
-    
+   
 
-    
-print("TESSSSSSSSSSSSSTING")
-
+print("###############@@@@@@@@@@@@@@@@@@@@@@##################@@@@@@@@@@@@@@@@@###########")
 
 for root, subdirs, files in os.walk(path):
     group = root[5:]
@@ -79,28 +74,38 @@ for root, subdirs, files in os.walk(path):
 
             letters = countLetters(text)
 
-            print(f"Group: {group}")
-            print(f"Results: ")
-    
-            # print("TRAIN")
-            # p1.training(letters,group, 23.4)
-            # p2.training(letters,group, 23.4)
-            # p3.training(letters,group, 23.4)
-            print("TEST")
-            print(p1.testing(letters))
-            print(p2.testing(letters))
-            print(p3.testing(letters))
+            print("TO TEST: ",group)
+            for p in perceptrons:
+                print(p.group,": ", p.testing(letters))
+
+            
+            # res = {}
+            # res[p1.group] = p1.testing(letters)
+            # res[p2.group] = p2.testing(letters)
+            # res[p3.group] = p3.testing(letters)
+
+            # result = p1.group
+            # max = res[p1.group]
+            # for key in res:
+            #     if res[key] > max:
+            #         result = key
+            #         max = res[key]
+
+            # print("ODP: ",result)
+            # print("###")
 
 
-while True:
-    inp = input("TEXT: ")
 
-    if inp.lower() == "exit":
-        break
 
-    let = countLetters(inp)
+# while True:
+#     inp = input("TEXT: ")
 
-    print(p1.testing(let))
-    print(p2.testing(let))
-    print(p3.testing(let))
+#     if inp.lower() == "exit":
+#         break
+
+#     let = countLetters(inp)
+
+#     print(p1.testing(let))
+#     print(p2.testing(let))
+#     print(p3.testing(let))
 
