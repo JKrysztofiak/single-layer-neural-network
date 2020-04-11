a1953e9e0995caeb8691a9283cf11e075efeeed7
Jakub Krysztofiak <jakub.krysztofiak@protonmail.com>
2020-04-11 14:38:16 +0200

    all perceptrons initialized with the same weights

--- Perceptron.py
+++ Perceptron.py
@@ -23,6 +23,7 @@ def add_vectors(v1: list,v2: list) -> list:
 
 def calculate_net(w: list, p: list) -> float:
     res = 0
+
     for i in range(0,len(w)):
         res += float(p[i])*float(w[i])
 
@@ -30,23 +31,31 @@ def calculate_net(w: list, p: list) -> float:
 
 
 def delta_rule(w: list, d: int, y: int, alfa: float, x: list, t: float) -> list:
-    
-    w.append(t)
-    x.append(-1)
 
-    w_prim = add_vectors(w, (multiply_vector(x,((d-y)*alfa))))
+    wp = w.copy()
+    xp = x.copy()
+
+    wp.append(t)
+    xp.append(-1)
+
+    w_prim = add_vectors(wp, (multiply_vector(xp,((d-y)*alfa))))
 
     return w_prim
 
         
 class Perceptron:
-    def __init__(self, k: int, group: str):
-        self.weights = [randint(-5,5) for x in range(0,k)]
-        self.t = randint(-5,5)
-        self.group = group
+    # def __init__(self, k: int, group: str):
+    #     self.weights = [randint(-10,10) for x in range(0,k)]
+    #     self.t = randint(-10,10)
+    #     self.group = group
 
+    def __init__(self, w: list, t: float, group: str):
+        self.weights = w.copy()
+        self.t = t
+        self.group = group
     
     def training(self, trainSet: list, trueRes: str, alfa: float):
+
         # net = calculate_net(self.weights, trainSet)
         net = calculate_net(multiply_vector(self.weights, (1.0/vector_length(self.weights))),multiply_vector(trainSet, (1.0/vector_length(trainSet))))
 
@@ -58,7 +67,7 @@ class Perceptron:
         # print(f"TrueRes: {trueRes} Perceptron: {self.group} res: {res}")
 
         
-        # i=0
+        i=0
 
         while (res == 0 and trueRes == self.group) or (res == 1 and trueRes != self.group):
                         
@@ -72,9 +81,13 @@ class Perceptron:
                 d = 0
                 y = 1
 
-            wprim = delta_rule(self.weights, d, y, alfa, trainSet, self.t)         
+            # wprim = delta_rule(self.weights, d, y, alfa, trainSet, self.t)
+            wprim = delta_rule(multiply_vector(self.weights, (1.0/vector_length(self.weights))), d, y, alfa, multiply_vector(trainSet, (1.0/vector_length(trainSet))), self.t)          
+            
             self.t = float(wprim[-1])
             self.weights = wprim[:-1]
+
+            
 
             # net = calculate_net(self.weights, trainSet)
             net = calculate_net(multiply_vector(self.weights, (1.0/vector_length(self.weights))),multiply_vector(trainSet, (1.0/vector_length(trainSet))))
@@ -91,7 +104,6 @@ class Perceptron:
         res = 0
         if net > self.t:
             res = 1
+
         # return net
         return res
-        
-        
